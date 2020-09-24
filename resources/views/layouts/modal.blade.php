2ea3939f2c471a95d61e5fed9531e13c3f66a41b
redbastie <redbastie@icloud.com>
2020-09-24 05:20:13 -0400

    modal lang

--- resources/views/layouts/modal.blade.php
+++ resources/views/layouts/modal.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">@yield('title')</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('Close') }}">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
